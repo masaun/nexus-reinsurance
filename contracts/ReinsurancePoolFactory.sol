198d3f770432809f18a48daf2a02c62d89544592
masaun <masanoriuno@gmail.com>
2020-12-01 17:04:25 +0900

    Add comments to 3 contracts

--- contracts/ReinsurancePoolFactory.sol
+++ contracts/ReinsurancePoolFactory.sol
@@ -3,7 +3,9 @@ pragma experimental ABIEncoderV2;
 
 
 /***
- * @notice - This contract that ...
+ * @notice - Template contract that can deploy reinsurance pools. 
+ *         - Important aspect for Nexus Mutual is that the template is configurable enough for the goals required.
+ * @dev - TO DO: add in detailed configurations, check if they meet Nexus requirements.
  **/
 contract ReinsurancePoolFactory {
 
