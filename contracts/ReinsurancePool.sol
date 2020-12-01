d058a7f622100425955fc354db16734bc5d12e2f
masaun <masanoriuno@gmail.com>
2020-12-01 17:10:58 +0900

    Define ReinsurancePool.sol

---
+++ contracts/ReinsurancePool.sol
@@ -0,0 +1,30 @@
+pragma solidity ^0.5.0;
+pragma experimental ABIEncoderV2;
+
+
+/***
+ * @notice - ReinsurancePool.sol is created via ReinsurancePoolFactory.sol
+ *         - Each ReinsurancePool has a contract address
+ **/
+contract ReinsurancePool {
+
+    constructor() public {}
+
+
+    ///------------------------------------------------------------
+    /// Internal functions
+    ///------------------------------------------------------------
+
+
+
+    ///------------------------------------------------------------
+    /// Getter functions
+    ///------------------------------------------------------------
+ 
+
+
+    ///------------------------------------------------------------
+    /// Private functions
+    ///------------------------------------------------------------
+
+}
