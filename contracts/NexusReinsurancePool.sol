2d9de22aa00046a5470900670f6a331a375bf891
masaun <masanoriuno@gmail.com>
2020-12-02 10:51:42 +0900

    Define claimForTakingLPToken()

--- contracts/NexusReinsurancePool.sol
+++ contracts/NexusReinsurancePool.sol
@@ -10,8 +10,20 @@ pragma experimental ABIEncoderV2;
  *         - X wNXM streamed per block split proportionally between all LP’s that aren’t in the un-lock period.
  **/
 contract NexusReinsurancePool {
-
     constructor() public {}
+
+
+    /***
+     * @notice - Claim triggers are defined if MCR% drops below a certain threshold (e.g. 80%) then a certain amount of LP tokens are taken and injected into Nexus Mutual as capital.
+     **/
+    function claimForTakingLPToken() public returns (bool) {
+        uint DefaultMCRRate = 80;  /// [Note]: 80%. This is temporary rate.
+        if (DefaultMCRRate < 80) {
+            /// [Todo]: a certain amount of LP tokens are taken and injected into Nexus Mutual as capital
+        }
+    }
+    
+
 
 
     ///------------------------------------------------------------
