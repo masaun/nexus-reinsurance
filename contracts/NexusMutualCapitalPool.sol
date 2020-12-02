5535ae2898cdfc2cbe4cd0e8d4e29acece07f64a
masaun <masanoriuno@gmail.com>
2020-12-02 22:25:09 +0900

    Define provideNXMReward() and receiveCapital() in NexusMutualCapitalPool.sol

--- contracts/NexusMutualCapitalPool.sol
+++ contracts/NexusMutualCapitalPool.sol
@@ -1,16 +1,45 @@
 pragma solidity ^0.5.0;
 pragma experimental ABIEncoderV2;
 
+/// [Note]: @openzeppelin/contracts v2.5.1
+import { IERC20 } from '@openzeppelin/contracts/token/ERC20/IERC20.sol';
 import { SafeMath } from "@openzeppelin/contracts/math/SafeMath.sol";
 
 
 /***
  * @notice - This is NexusMutualCapitalPool contract
+ *         - Provides NXM rewards.
+ *         - Receives capital in the event of a claim.
+ *         - Formal governance to instruct the multi-sig operators of the reinsurance pool manager.
  **/
 contract NexusMutualCapitalPool {
     using SafeMath for uint;
 
-    constructor() public {}
+    IERC20 public dai;
+
+    constructor(address _dai) public {
+        dai = IERC20(_dai);
+    }
+
+    /***
+     * @notice - Provides NXM rewards.
+     **/
+    function provideNXMReward() public returns (bool) {}
+    
+
+    /***
+     * @notice - Receives capital (underlying asset) in the event of a claim. 
+     **/
+    function receivesCapital(uint underlyingAssetAmount) public returns (bool) {
+        dai.transferFrom(msg.sender, address(this), underlyingAssetAmount);
+    }
+    
+
+    /***
+     * @notice - Formal governance to instruct the multi-sig operators of the reinsurance pool manager.
+     **/
+
+
 
 
     ///------------------------------------------------------------
