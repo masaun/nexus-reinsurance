96eba213eefde5b6b326e14f3cba3580f9d3768d
masaun <masanoriuno@gmail.com>
2020-12-02 23:37:21 +0900

    Add nxmToken.transfer() in provideNXMReward() of NexusMutualCapitalPool.sol

--- contracts/NexusMutualCapitalPool.sol
+++ contracts/NexusMutualCapitalPool.sol
@@ -2,8 +2,12 @@ pragma solidity ^0.5.0;
 pragma experimental ABIEncoderV2;
 
 /// [Note]: @openzeppelin/contracts v2.5.1
+
 import { IERC20 } from '@openzeppelin/contracts/token/ERC20/IERC20.sol';
 import { SafeMath } from "@openzeppelin/contracts/math/SafeMath.sol";
+
+import { NXMToken } from "./nexus-mutual/modules/token/NXMToken.sol";
+import { NexusReinsurancePoolManager } from "./NexusReinsurancePoolManager.sol";
 
 
 /***
@@ -15,22 +19,35 @@ import { SafeMath } from "@openzeppelin/contracts/math/SafeMath.sol";
 contract NexusMutualCapitalPool {
     using SafeMath for uint;
 
+    NXMToken public nxmToken;
+    NexusReinsurancePoolManager public nexusReinsurancePoolManager;
     IERC20 public dai;
 
-    constructor(address _dai) public {
-        dai = IERC20(_dai);
+    address NXM_TOKEN;
+    address NEXUS_REINSURANCE_POOL_MANAGER;
+
+    constructor(NXMToken _nxmToken, NexusReinsurancePoolManager _nexusReinsurancePoolManager, IERC20 _dai) public {
+        nxmToken = _nxmToken;
+        nexusReinsurancePoolManager = _nexusReinsurancePoolManager;
+        dai = IERC20(address(_dai));
+
+        NXM_TOKEN = address(_nxmToken);
+        NEXUS_REINSURANCE_POOL_MANAGER = address(_nexusReinsurancePoolManager);
     }
 
+
     /***
-     * @notice - Provides NXM rewards.
+     * @notice - Provides NXM rewards (into the NexusReinsurancePoolManager contract)
      **/
-    function provideNXMReward() public returns (bool) {}
+    function provideNXMReward(uint nxmAmount) public returns (bool) {
+        nxmToken.transfer(NEXUS_REINSURANCE_POOL_MANAGER, nxmAmount);
+    }
     
 
     /***
      * @notice - Receives capital (underlying asset) in the event of a claim. 
      **/
-    function receivesCapital(uint underlyingAssetAmount) public returns (bool) {
+    function receiveCapital(uint underlyingAssetAmount) public returns (bool) {
         dai.transferFrom(msg.sender, address(this), underlyingAssetAmount);
     }
     
