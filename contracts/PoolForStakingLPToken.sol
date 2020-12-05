dc1803624cc7d0823397919171801890ed78c1fa
masaun <masanoriuno@gmail.com>
2020-12-05 22:46:59 +0900

    Define PoolForStakingLPToken.sol

---
+++ contracts/PoolForStakingLPToken.sol
@@ -0,0 +1,14 @@
+pragma solidity ^0.5.0;
+pragma experimental ABIEncoderV2;
+
+
+/***
+ * @notice - Users can stake Uniswap/Balancer LP tokens in return for additional wNXM rewards .
+ *         - Staked LP tokens into this Pool contract.
+ *         - Staked LP tokens must be only high quality tokens such as ETH/DAI, ETH/USDC, etc...
+ **/
+contract PoolForStakingLPToken {
+ 
+    constructor() public {}
+
+}
